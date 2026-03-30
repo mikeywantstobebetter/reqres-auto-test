a0181ece30fcf78deddc4e57e5ecab3155ef89e8
mikeywantstobebetter <mikepan0320@gmail.com>
2026-03-30 21:59:54 +0800

    init:初始化项目目录结构

---
+++ config_example.py
@@ -0,0 +1,6 @@
+#config_example.py - 配置文件模版
+#复制此文件并命名为 config.py，其中填入真实的值
+BASE_URL = "https://reqres.in"
+API_KEY = "your_api_key_here"
+LOGIN_EMAIL = "your_email_here"
+LOGIN_PASSWORD = "your_password_here"
