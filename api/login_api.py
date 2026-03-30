0aa46ebcc10026f987f301d882e2838daf8f8f0a
mikeywantstobebetter <mikepan0320@gmail.com>
2026-03-30 23:35:49 +0800

    feat: 添加ogin 和register的接口封装

---
+++ api/login_api.py
@@ -0,0 +1,17 @@
+import requests
+import config
+def login(session, email, password):
+    data = {
+        "email":email,
+        "password":password
+    }
+    response = session.post(f"{config.BASE_URL}/api/login",json = data)
+    return response
+
+def register(session, email, password):
+    data = {
+        "email":email,
+        "password":password
+    }
+    response = session.post(f"{config.BASE_URL}/api/register",json = data)
+    return response
